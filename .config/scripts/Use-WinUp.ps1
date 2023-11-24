6130e3f75a8c5558b614dfaf4a37727754f2dd7f
RustyTake-Off <109174317+RustyTake-Off@users.noreply.github.com>
2023-11-24 21:01:12 +0100

    Add script Use-WinUp

---
+++ .config/scripts/Use-WinUp.ps1
@@ -0,0 +1,20 @@
+﻿# ╒══════╗        ╒═══════╗
+# │ ╓──┐ ║════════╗  ╓─┐  ║
+# │ ╚══╛ ║──┐  ╓──╜  ║ │  ║  RustyTake-Off
+# │ ╓─┐ ╔╝  │  ║  │  ║ │  ║  https://github.com/RustyTake-Off
+# │ ║ │ ╚╗  │  ║  │  ╚═╛  ║
+# └─╜ └──╜  └──╜  └───────╜
+# WinUp - script for setting up Windows.
+
+<#
+Install all updates (manually)
+Install all drivers (manually)
+Install fonts
+Invoke CTT
+Install PowerShell modules
+Copy ssh keys
+Install base apps
+Install util apps
+#>
+
+#Requires -RunAsAdministrator
