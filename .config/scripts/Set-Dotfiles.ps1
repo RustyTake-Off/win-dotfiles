1fa8004c13bc038a6ad5ed96f528195804e047ea
RustyTake-Off <109174317+RustyTake-Off@users.noreply.github.com>
2023-11-24 21:43:11 +0100

    Add script setting up dotfiles

---
+++ .config/scripts/Set-Dotfiles.ps1
@@ -0,0 +1,156 @@
+﻿# ╒══════╗        ╒═══════╗
+# │ ╓──┐ ║════════╗  ╓─┐  ║
+# │ ╚══╛ ║──┐  ╓──╜  ║ │  ║  RustyTake-Off
+# │ ╓─┐ ╔╝  │  ║  │  ║ │  ║  https://github.com/RustyTake-Off
+# │ ║ │ ╚╗  │  ║  │  ╚═╛  ║
+# └─╜ └──╜  └──╜  └───────╜
+# Script for setting up Windows dotfiles.
+
+#Requires -RunAsAdministrator
+
+$ConfigPowerShellProfilePath = "$env:USERPROFILE\.config\powershell_profile"
+$ConfigScriptsPath = "$env:USERPROFILE\.config\scripts"
+$ConfigWindowsTerminalPath = "$env:USERPROFILE\.config\windows_terminal"
+$ConfigWingetPath = "$env:USERPROFILE\.config\winget"
+$ConfigWSLPath = "$env:USERPROFILE\.config\wsl"
+
+function New-SymLink([String]$SourceToLink, [String]$TargetToLink) {
+    New-Item -ItemType SymbolicLink -Target $SourceToLink -Path $TargetToLink
+    Write-Output "Creating SymLink: $($(Split-Path -Path $SourceToLink) -replace [Regex]::Escape($env:USERPROFILE), '...')\$((Get-Item $SourceToLink).Name) -> $($(Split-Path -Path $TargetToLink) -replace [Regex]::Escape($env:USERPROFILE), '...')\$((Get-Item $TargetToLink).Name)"
+}
+
+function New-HashThenSymLink([String]$SourceToLink, [String]$TargetToLink) {
+    $HashOne = Get-FileHash -Path $SourceToLink -Algorithm SHA256
+    $HashTwo = Get-FileHash -Path $TargetToLink -Algorithm SHA256
+
+    if ($HashOne.Hash -ne $HashTwo.Hash) {
+        Remove-Item -Path $TargetToLink -Force
+        Write-Output "Removing $($(Split-Path -Path $TargetToLink) -replace [Regex]::Escape($env:USERPROFILE), '...')\$((Get-Item $TargetToLink).Name)"
+        New-SymLink -SourceToLink $SourceToLink -TargetToLink $TargetToLink
+    } else {
+        Write-Output "SymLink already set: $($(Split-Path -Path $SourceToLink) -replace [Regex]::Escape($env:USERPROFILE), '...')\$((Get-Item $SourceToLink).Name) -> $($(Split-Path -Path $TargetToLink) -replace [Regex]::Escape($env:USERPROFILE), '...')\$((Get-Item $TargetToLink).Name)"
+    }
+}
+
+function Invoke-SetSymLinks([String]$SourceToLink, [String]$TargetToLink) {
+    if ((Test-Path -Path $SourceToLink -PathType Leaf) -and (-not (Test-Path -Path $TargetToLink -PathType Leaf))) {
+        New-SymLink -SourceToLink $SourceToLink -TargetToLink $TargetToLink
+    } elseif ((Test-Path -Path $SourceToLink -PathType Leaf) -and (Test-Path -Path $TargetToLink -PathType Leaf)) {
+        New-HashThenSymLink -SourceToLink $SourceToLink -TargetToLink $TargetToLink
+    } elseif ((-not (Test-Path -Path $SourceToLink -PathType Leaf)) -and (Test-Path -Path $TargetToLink -PathType Leaf)) {
+        Write-Error "Cannot create SymLink SourceToLink doesn't exist in dotfiles"
+    }
+}
+
+if (-not (Test-Path -Path "$env:USERPROFILE\pr" -PathType Container)) {
+    New-Item -Path "$env:USERPROFILE\pr" -ItemType Directory
+    Write-Output "Creating 'work' folder"
+}
+
+if (-not (Test-Path -Path "$env:USERPROFILE\wk" -PathType Container)) {
+    New-Item -Path "$env:USERPROFILE\wk" -ItemType Directory
+    Write-Output "Creating 'personal' folder"
+}
+
+if (-not (Test-Path -Path "$env:USERPROFILE\.dotfiles" -PathType Container)) {
+    if (Get-Command git) {
+        git clone --bare 'https://github.com/RustyTake-Off/win-dotfiles.git' "$env:USERPROFILE\.dotfiles"
+        git --git-dir="$env:USERPROFILE\.dotfiles" --work-tree=$env:USERPROFILE checkout
+        git --git-dir="$env:USERPROFILE\.dotfiles" --work-tree=$env:USERPROFILE config status.showUntrackedFiles no
+    } else {
+        Write-Error 'Git is not installed'
+        Exit
+    }
+} else {
+    Write-Output 'Dotfiles are set. Checking for updates'
+    git --git-dir="$env:USERPROFILE\.dotfiles" --work-tree=$env:USERPROFILE reset --hard
+    git --git-dir="$env:USERPROFILE\.dotfiles" --work-tree=$env:USERPROFILE pull
+}
+
+$PowerShellProfilePath = "$env:USERPROFILE\Documents\PowerShell"
+if ($ConfigPowerShellProfileFiles = Get-ChildItem -Path $ConfigPowerShellProfilePath -File -Recurse) {
+    if (-not (Test-Path -Path $PowerShellProfilePath -PathType Container)) {
+        New-Item -Path $PowerShellProfilePath -ItemType Directory
+        foreach ($File in $ConfigPowerShellProfileFiles) {
+            Invoke-SetSymLinks -SourceToLink "$ConfigPowerShellProfilePath\$($File.Name)" -TargetToLink "$PowerShellProfilePath\$($File.Name)"
+        }
+    } else {
+        foreach ($File in $ConfigPowerShellProfileFiles) {
+            Invoke-SetSymLinks -SourceToLink "$ConfigPowerShellProfilePath\$($File.Name)" -TargetToLink "$PowerShellProfilePath\$($File.Name)"
+        }
+    }
+} else {
+    Write-Output 'PowerShell profile config is missing from dotfiles'
+}
+
+$PowerShellScriptsPath = "$env:USERPROFILE\Documents\PowerShell\Scripts"
+if ($ConfigScriptFiles = Get-ChildItem -Path $ConfigScriptsPath -File -Recurse) {
+
+    if (-not (Test-Path -Path $PowerShellScriptsPath -PathType Container)) {
+        New-Item -Path $PowerShellScriptsPath -ItemType Directory
+
+        foreach ($File in $ConfigScriptFiles) {
+            Invoke-SetSymLinks -SourceToLink "$ConfigScriptsPath\$($File.Name)" -TargetToLink "$PowerShellScriptsPath\$($File.Name)"
+        }
+    } else {
+        foreach ($File in $ConfigScriptFiles) {
+            Invoke-SetSymLinks -SourceToLink "$ConfigScriptsPath\$($File.Name)" -TargetToLink "$PowerShellScriptsPath\$($File.Name)"
+        }
+    }
+} else {
+    Write-Output 'PowerShell scripts are missing from dotfiles'
+}
+
+$WindowsTerminalPath = "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
+if ($ConfigWindowsTerminalFiles = Get-ChildItem -Path $ConfigWindowsTerminalPath -File -Recurse) {
+
+    if (-not (Test-Path -Path $WindowsTerminalPath -PathType Container)) {
+        New-Item -Path $WindowsTerminalPath -ItemType Directory
+
+        foreach ($File in $ConfigWindowsTerminalFiles) {
+            Invoke-SetSymLinks -SourceToLink "$ConfigWindowsTerminalPath\$($File.Name)" -TargetToLink "$WindowsTerminalPath\settings.json"
+        }
+    } else {
+        foreach ($File in $ConfigWindowsTerminalFiles) {
+            Invoke-SetSymLinks -SourceToLink "$ConfigWindowsTerminalPath\$($File.Name)" -TargetToLink "$WindowsTerminalPath\settings.json"
+        }
+    }
+} else {
+    Write-Output 'Windows Terminal config is missing from dotfiles'
+}
+
+$WingetPath = "$env:LOCALAPPDATA\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState"
+if ($ConfigWingetFiles = Get-ChildItem -Path $ConfigWingetPath -File -Recurse) {
+
+    if (-not (Test-Path -Path $WingetPath -PathType Container)) {
+        New-Item -Path $WingetPath -ItemType Directory
+
+        foreach ($File in $ConfigWingetFiles) {
+            Invoke-SetSymLinks -SourceToLink "$ConfigWingetPath\$($File.Name)" -TargetToLink "$WingetPath\settings.json"
+            Invoke-SetSymLinks -SourceToLink "$ConfigWingetPath\$($File.Name)" -TargetToLink "$WingetPath\settings.json.backup"
+        }
+    } else {
+        foreach ($File in $ConfigWingetFiles) {
+            Invoke-SetSymLinks -SourceToLink "$ConfigWingetPath\$($File.Name)" -TargetToLink "$WingetPath\settings.json"
+            Invoke-SetSymLinks -SourceToLink "$ConfigWingetPath\$($File.Name)" -TargetToLink "$WingetPath\settings.json.backup"
+        }
+    }
+} else {
+    Write-Output 'Winget config is missing from dotfiles'
+}
+
+if ($ConfigWSLFiles = Get-ChildItem -Path $ConfigWSLPath -File -Recurse) {
+
+    if (-not (Test-Path -Path "$env:USERPROFILE\.wslconfig" -PathType Leaf)) {
+
+        foreach ($File in $ConfigWSLFiles) {
+            Invoke-SetSymLinks -SourceToLink "$ConfigWSLPath\$($File.Name)" -TargetToLink "$env:USERPROFILE\.wslconfig"
+        }
+    } else {
+        foreach ($File in $ConfigWSLFiles) {
+            Invoke-SetSymLinks -SourceToLink "$ConfigWSLPath\$($File.Name)" -TargetToLink "$env:USERPROFILE\.wslconfig"
+        }
+    }
+} else {
+    Write-Output 'WSL config is missing from dotfiles'
+}
